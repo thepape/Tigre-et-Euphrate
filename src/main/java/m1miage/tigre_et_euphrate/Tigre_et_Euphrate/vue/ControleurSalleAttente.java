aa0528af9f59e729dd5c1470f0be6c58af3acfb6
jerome54 <jerome.rollinger@gmail.com>
2016-04-18 18:42:52 +0200

    feat(ControlleurSalleAttente) : Ajout bouton
    Ajout des boutons pour les dynastie
    Ajout de l'entete de la methode de mis a jour

--- src/main/java/m1miage/tigre_et_euphrate/Tigre_et_Euphrate/vue/ControleurSalleAttente.java
+++ src/main/java/m1miage/tigre_et_euphrate/Tigre_et_Euphrate/vue/ControleurSalleAttente.java
@@ -1,16 +1,67 @@
 package m1miage.tigre_et_euphrate.Tigre_et_Euphrate.vue;
 
+import javafx.scene.control.Button;
 import javafx.fxml.FXML;
 
+/**
+ * Controlleur de la salle d'attente
+ * @author 
+ *
+ */
 public class ControleurSalleAttente {
+	
+	/**
+	 * Application gérer par ce controleur
+	 */
+	private MainApp mainApp;
+	
+	//Bouton des differentes dynasties
+	/**
+	 * Bouton de la dynastie lanister
+	 */
+	@FXML
+	private Button lanister;
+	
+	/**
+	 * Bouton de la dynastie stark
+	 */
+	@FXML
+	private Button stark;
+	
+	/**
+	 * Bouton de la dynastie targaryen
+	 */
+	@FXML
+	private Button targaryen;
+	
+	/**
+	 * Bouton de la dynastie tyrell
+	 */
+	@FXML
+	private Button tyrell;
+	
 
+	/**
+	 * Constructeur
+	 */
 	public ControleurSalleAttente() {
 	}
 	
 	
+	// Fonctions
 	
 	@FXML
 	public void retourAuMenu(){
 		MainApp.getInstance().goToMenuPage();
 	}
+	
+	/**
+	 * Fonction de mis a jour lorsqu'une dynastie est choisi
+	 */
+	@FXML
+	public void dynastieChoisi(){
+		
+	}
+	
+	
 }
